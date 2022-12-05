00ddd5e2309144bf23826718f2848ec7f4149691
vladgarmacea <vlad.garmacea@gmail.com>
2022-12-05 14:21:36 +0200

    Add files via upload

---
+++ models.py
@@ -0,0 +1,160 @@
+
+import warnings
+
+import matplotlib
+
+warnings.filterwarnings('ignore')
+
+import os
+import shutil
+import datetime
+import gc
+from tqdm import tqdm
+
+import pandas as pd
+import numpy as np
+
+#%matplotlib inline
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set(style='whitegrid')
+
+
+import scipy
+from scipy import hstack
+
+
+from sklearn.metrics import mean_squared_error as mse
+from math import sqrt
+from sklearn.linear_model import Ridge
+from sklearn.svm import SVR
+from sklearn.ensemble import RandomForestRegressor
+from lightgbm import LGBMRegressor
+
+from sklearn.model_selection import RandomizedSearchCV
+from scipy.stats import randint as sp_randint
+from scipy.stats import uniform
+from sklearn.feature_selection.univariate_selection import SelectKBest, f_regression
+
+from google.colab import drive
+drive.mount('/content/drive')
+os.chdir('/content/drive/My Drive/Colab Notebooks/MercariCS')
+
+X_train = scipy.sparse.load_npz("train_final.npz")
+y_train = np.load('y_train.npy')
+
+X_cv = scipy.sparse.load_npz("cv_final.npz")
+y_cv = np.load('y_cv.npy')
+
+# X_test = scipy.sparse.load_npz("test_final.npz")
+X_train.shape, y_train.shape, X_cv.shape, y_cv.shape
+
+c_param = [0.01, 0.03, 0.1, 0.3, 1, 3, 10]
+cv_rmsle_array=[]
+
+for i in c_param:
+    model = SVR(C=i, max_iter=200)
+    model.fit(X_train, y_train)
+    preds_cv = model.predict(X_cv)
+    cv_err = sqrt(mse(y_cv, preds_cv))
+    cv_rmsle_array.append(cv_err)
+
+    print('RMSLE for C=',i, 'is', cv_err)
+
+best_C = np.argmin(cv_rmsle_array)
+
+fig, ax = plt.subplots()
+ax.plot(c_param, cv_rmsle_array)
+ax.scatter(c_param, cv_rmsle_array)
+for i, txt in enumerate(np.round(cv_rmsle_array,3)):
+    ax.annotate((c_param[i],np.round(txt,3)), (c_param[i],cv_rmsle_array[i]))
+plt.title("Cross Validation Error for each C")
+plt.xlabel("C")
+plt.ylabel("Error")
+plt.show()
+best_C = c_param[best_C]
+print('Best C: ', best_C)
+model = SVR(C=best_C, max_iter=200)
+model.fit(X_train, y_train)
+svr_preds_tr = model.predict(X_train)
+svr_preds_cv = model.predict(X_cv)
+print('Train RMSLE:', sqrt(mse(y_train, svr_preds_tr)))
+
+svr_rmsle = sqrt(mse(y_cv, svr_preds_cv))
+print("Cross validation RMSLE: ", svr_rmsle)
+
+dt = datetime.datetime #to track time
+
+estimators = [10, 20]
+cv_rmsle_array=[]
+t0 = dt.now()
+for i in estimators:
+    model = RandomForestRegressor(n_estimators=i, min_samples_split=10, max_features='sqrt', max_samples=0.9, random_state=42)
+    model.fit(X_train, y_train)
+    preds_cv = model.predict(X_cv)
+    cv_err = sqrt(mse(y_cv, preds_cv))
+    cv_rmsle_array.append(cv_err)
+
+    print (f'RMSLE for N={i} is {cv_err}\t elapsed time:{dt.now() - t0}')
+
+best_N = np.argmin(cv_rmsle_array)
+
+fig, ax = plt.subplots()
+ax.plot(estimators, cv_rmsle_array)
+ax.scatter(estimators, cv_rmsle_array)
+for i, txt in enumerate(np.round(cv_rmsle_array,3)):
+    ax.annotate((estimators[i],np.round(txt,3)), (estimators[i],cv_rmsle_array[i]))
+plt.title("Cross Validation Error for each N")
+plt.xlabel("n_estimators")
+plt.ylabel("Error")
+plt.show()
+best_N = estimators[best_N]
+print('Best n_estimators: ', best_N)
+model = RandomForestRegressor(n_estimators=best_N, min_samples_split=10, max_features='sqrt', max_samples=0.9, random_state=42)
+model.fit(X_train, y_train)
+rf_preds_tr = model.predict(X_train)
+rf_preds_cv = model.predict(X_cv)
+# rf_preds_te = model.predict(X_test)
+
+print('Train RMSLE:', sqrt(mse(y_train, rf_preds_tr)))
+
+rf_rmsle = sqrt(mse(y_cv, rf_preds_cv))
+print("Cross validation RMSLE: ", rf_rmsle)
+lgb_model = LGBMRegressor(subsample=0.9)
+
+params = {'learning_rate': uniform(0, 1),
+          'n_estimators': sp_randint(200, 1500),
+          'num_leaves': sp_randint(20, 200),
+          'max_depth': sp_randint(2, 15),
+          'min_child_weight': uniform(0, 2),
+          'colsample_bytree': uniform(0, 1),
+         }
+lgb_random = RandomizedSearchCV(lgb_model, param_distributions=params, n_iter=10, cv=3, random_state=42,
+                                scoring='neg_root_mean_squared_error', verbose=10, return_train_score=True)
+lgb_random = lgb_random.fit(X_train, y_train)
+df1 = pd.DataFrame(lgb_random.cv_results_)
+df1[['param_learning_rate', 'param_n_estimators', 'param_num_leaves', 'param_max_depth', 'param_min_child_weight', 'param_colsample_bytree', 'mean_train_score', 'mean_test_score']]
+best_params = lgb_random.best_params_
+print(best_params)
+model = LGBMRegressor(**best_params, subsample=0.9, random_state=42, n_jobs=-1)
+model.fit(X_train, y_train)
+
+X_cv = scipy.sparse.load_npz("cv_final.npz")
+y_cv = np.load('y_cv.npy')
+
+X_test = scipy.sparse.load_npz("test_final.npz")
+lgb_preds_tr = model.predict(X_train)
+lgb_preds_cv = model.predict(X_cv)
+lgb_preds_te = model.predict(X_test)
+
+print('Train RMSLE:', sqrt(mse(y_train, lgb_preds_tr)))
+
+lgb_rmsle = sqrt(mse(y_cv, lgb_preds_cv))
+print("Cross validation RMSLE: ", lgb_rmsle)
+submission_df = pd.read_csv('ridge_submission.csv')
+
+submission_df['price'] = np.exp(lgb_preds_te) - 1
+
+submission_df.to_csv('lgb_submission.csv', index=False)
+
+
